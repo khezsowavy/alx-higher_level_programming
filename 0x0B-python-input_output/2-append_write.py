cd33476d9342b9241d48718af64ea7eae2aa8726
Khez <106893248+khezsowavy@users.noreply.github.com>
2024-02-20 13:30:20 +0100

    Update 2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,13 +1,14 @@
 !/usr/bin/python3
 """ Module that contains a function that appends to a text file """
-def append_write(filename="", text=""):
-    """ Function that appends to a text file
-    Args:
-        filename: filename
-        text: text to write
-    Raises
-        Exception: when the file can be opened
-    """
 
-    with open(filename, 'a', encoding="utf-8") as f:
+def append_write(filename="", text=""):
+    """Appends a string to the end of a UTF8 text file.
+
+    Args:
+        filename (str): The name of the file to append to.
+        text (str): The string to append to the file.
+    Returns:
+        The number of characters appended.
+    """
+    with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
