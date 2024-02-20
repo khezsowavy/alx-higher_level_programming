8ae4f0184ef8706f7825000fe493588679ab236c
Khez <106893248+khezsowavy@users.noreply.github.com>
2024-02-20 14:17:32 +0100

    Update 9-student.py

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
 """ Module that defines the class Student """
 
+
 class Student:
     """ Class to create student instances """
 
     def __init__(self, first_name, last_name, age):
-        """ Special method to initialize """
+        """Initialize a new Student.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """ Method that returns directory description """
-        return self.__dict__.copy()
+        """Get a dictionary representation of the Student."""
+        return self.__dict__
