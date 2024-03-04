ae23132028d112cbe58ed2da6f60825e27879162
Khez <106893248+khezsowavy@users.noreply.github.com>
2024-03-04 11:33:21 +0100

    Update 3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-def safe_print_list_integers(my_list=[], x=0):
-    ret = 0
-    for i in range(0, x):
-        try:
-            print("{:d}".format(my_list[i]), end="")
-            ret += 1
-        except (ValueError, TypeError):
-            continue
-            finally:
-    print("")
+def safe_print_division(a, b):
+    """Returns the division of a by b."""
+    try:
+        ret = a / b
+    except (TypeError, ZeroDivisionError):
+        ret = None
+    finally:
+        print("Inside result: {}".format(ret))
     return (ret)
